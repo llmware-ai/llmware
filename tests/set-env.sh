7c3d08f093fbc0af16dc1cd1fa37cb6244779d60
Jessica Berliner <jberliner@aibloks.com>
2023-11-21 17:41:12 -0500

    add env template

---
+++ tests/set-env.sh
@@ -0,0 +1,15 @@
+# LLM API KEYS 
+#export OPENAI_API_KEY=
+#export ANTHROPIC_API_KEY=
+#export AI21_API_KEY=
+#export COHERE_API_KEY=
+#export READ_GPT_API_KEY=
+#export GOOGLE_API_KEY=<this value must be the full text of your .json credential file...eg. $(echo creds.json)>
+
+# DB KEYS AND CONNCTION STRINGS
+#export COLLECTION_DB_URI=<an alternative mongo connection string>
+#export MONGO_ATLAS_CONNECTION_URI=<a MongoDB Atlas connection sting>
+#export MILVUS_HOST=<an alternative Milvus host>
+#export MILVUS_PORT=<an alternative Milvus port>
+#export PINECONE_API_KEY=
+#export PINECONE_ENVIRONMENT=<for example 'gcp-starter'>
