a6b0b41da360e71f6c6d77344e9f961480b2ac59
Darren Oberst <41238031+doberst@users.noreply.github.com>
2023-12-18 07:01:27 -0500

    Update contract_analysis_on_laptop_with_bling_models.py

--- examples/RAG/contract_analysis_on_laptop_with_bling_models.py
+++ examples/RAG/contract_analysis_on_laptop_with_bling_models.py
@@ -1,6 +1,6 @@
 
-"""This example demonstrats doing an analysis across contracts entirely on on a laptop
-    using local models
+"""This example demonstrates a basic contract analysis workflow run entirely on on a laptop
+    using a RAG-finetuned small specialized instruct BLING model
 """
 
 import os
@@ -10,7 +10,7 @@ from llmware.setup import Setup
 from llmware.configs import LLMWareConfig
 
 
-def contract_analysis_on_laptop (model_name, from_hf=False):
+def contract_analysis_on_laptop (model_name):
 
     # Load the llmware sample files
     print (f"\n > Loading the llmware sample files...")
@@ -23,34 +23,31 @@ def contract_analysis_on_laptop (model_name, from_hf=False):
                   "governing law": "What is the governing law?"}
 
     print (f"\n > Loading model {model_name}...")
-    # Note: Some newer models use local custom code in their HF repos which is not trusted by default
-    #  For now, you can pass in a dummy api_key and we'll set the right config to trust that code
-    #  This will likely be changing in the future
-    if from_hf:
-        # local cpu open source model
-        prompter = Prompt().load_model(model_name,from_hf=True)
-    else:
-        # e.g., 'gpt-4'
-        prompter = Prompt().load_model(model_name)
+
+    prompter = Prompt().load_model(model_name)
 
     for i, contract in enumerate(os.listdir(contracts_path)):
 
-        print("\nAnalyzing contract: ", str(i+1), contract)
+        # exclude potential mac os created file artifact in folder path
+        if contract != ".DS_Store":
+            
+            print("\nAnalyzing contract: ", str(i+1), contract)
 
-        print("LLM Responses:")
-        for key, value in query_list.items():
+            print("LLM Responses:")
+            
+            for key, value in query_list.items():
 
-            # contract is parsed, text-chunked, and then filtered by topic key
-            source = prompter.add_source_document(contracts_path, contract, query=key)
+                # contract is parsed, text-chunked, and then filtered by topic key
+                source = prompter.add_source_document(contracts_path, contract, query=key)
 
-            # calling the LLM with 'source' information from the contract automatically packaged into the prompt
-            responses = prompter.prompt_with_source(value, prompt_name="just_the_facts", temperature=0.3)
+                # calling the LLM with 'source' information from the contract automatically packaged into the prompt
+                responses = prompter.prompt_with_source(value, prompt_name="just_the_facts", temperature=0.3)
 
-            for r, response in enumerate(responses):
-                print(key, ":", re.sub("[\n]"," ", response["llm_response"]).strip())
+                for r, response in enumerate(responses):
+                    print(key, ":", re.sub("[\n]"," ", response["llm_response"]).strip())
 
-            # We're done with this contract, clear the source from the prompt
-            prompter.clear_source_materials()
+                # We're done with this contract, clear the source from the prompt
+                prompter.clear_source_materials()
 
     # Save jsonl report to jsonl to /prompt_history folder
     print("\nPrompt state saved at: ", os.path.join(LLMWareConfig.get_prompt_path(),prompter.prompt_id))
