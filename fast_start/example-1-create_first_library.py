eaec384b06b90230c5207e0464331af4b387f8d7
DARREN OBERST <darrenoberst@DARRENs-MacBook-Pro.local>
2024-01-24 09:47:16 -0500

    fast start kit of step-by-step examples

---
+++ fast_start/example-1-create_first_library.py
@@ -0,0 +1,128 @@
+
+""" Fast Start Example #1 - Library - converting document files into an indexed knowledge collection.
+
+    In this example, we will illustrate a basic recipe for completing the following steps:
+
+      1. Create a library as a organizing construct for your knowledge-base
+      2. Download sample files for a Fast Start - easy to 'swap out' and replace with your own files
+      3. Use library.add_files method to automatically parse, text chunk and index the documents
+      4. Run a basic text query against your new Library
+"""
+
+import os
+from llmware.library import Library
+from llmware.retrieval import Query
+from llmware.setup import Setup
+from llmware.configs import LLMWareConfig
+
+
+def parsing_documents_into_library(library_name, sample_folder):
+
+    print(f"\nExample - Parsing Files into Library")
+
+    #   create new library
+    print (f"\nStep 1 - creating library {library_name}")
+    library = Library().create_new_library(library_name)
+
+    #   load the llmware sample files
+    #   -- note: if you have used this example previously, UN-Resolutions-500 is new path
+    #   -- to pull updated sample files, set: 'over_write=True'
+
+    sample_files_path = Setup().load_sample_files(over_write=False)
+    print (f"Step 2 - loading the llmware sample files and saving at: {sample_files_path}")
+
+    #   note: to replace with your own documents, just point to a local folder path that has the documents
+    ingestion_folder_path = os.path.join(sample_files_path, sample_folder)
+
+    print (f"Step 3 - parsing and indexing files from {ingestion_folder_path}")
+
+    #   add files is the key ingestion method - parses, text chunks and indexes all files in folder
+    #       --will automatically route to correct parser based on file extension
+    #       --supported file extensions:  .pdf, .pptx, .docx, .xlsx, .csv, .md, .txt, .json, .wav, and .zip, .jpg, .png
+
+    parsing_output = library.add_files(ingestion_folder_path)
+
+    print (f"Step 4 - completed parsing - {parsing_output}")
+
+    #   check the updated library card
+    updated_library_card = library.get_library_card()
+    doc_count = updated_library_card["documents"]
+    block_count = updated_library_card["blocks"]
+    print(f"Step 5 - updated library card - documents - {doc_count} - blocks - {block_count} - {updated_library_card}")
+
+    #   check the main folder structure created for the library - check /images to find extracted images
+    library_path = library.library_main_path
+    print(f"Step 6 - library artifacts - including extracted images - saved at folder path - {library_path}")
+
+    #   use .add_files as many times as needed to build up your library, and/or create different libraries for
+    #   different knowledge bases
+
+    #   now, your library is ready to go and you can start to use the library for running queries
+    #   if you are using the "Agreements" library, then a good easy 'hello world' query is "base salary"
+    #   if you are using one of the other sample folders (or your own), then you should adjust the query
+
+    #   queries are always created the same way, e.g., instantiate a Query object, and pass a library object
+    #   --within the Query class, there are a variety of useful methods to run different types of queries
+
+    test_query = "base salary"
+
+    print(f"\nStep 7 - running a test query - {test_query}\n")
+
+    query_results = Query(library).text_query(test_query, result_count=10)
+
+    for i, result in enumerate(query_results):
+
+        #   note: each result is a dictionary with a wide range of useful keys
+        #   -- we would encourage you to take some time to review each of the keys and the type of metadata available
+
+        #   here are a few useful attributes
+        text = result["text"]
+        file_source = result["file_source"]
+        page_number = result["page_num"]
+        doc_id = result["doc_ID"]
+        block_id = result["block_ID"]
+        matches = result["matches"]
+
+        #   -- if print to console is too verbose, then pick just a few keys for print
+        print("query results: ", i, result)
+
+    return parsing_output
+
+
+if __name__ == "__main__":
+
+    #  note on sample documents - downloaded by Setup()
+    #       UN-Resolutions-500 is 500 pdf documents
+    #       Invoices is 40 pdf invoice samples
+    #       Agreements is ~15 contract documents
+    #       AgreementsLarge is ~80 contract documents
+    #       FinDocs is ~15 financial annual reports and earnings
+    #       SmallLibrary is a mix of ~10 pdf and office documents
+
+    #   optional - set the active DB to be used - by default, it is "mongo"
+    #   if you are just getting started, and have not installed a separate db, select "sqlite"
+
+    LLMWareConfig().set_active_db("sqlite")
+
+    #   if you want to see a different log view, e.g., see a list of each parsed files 'in progress',
+    #   you can set a different debug mode view anytime
+
+    #   debug_mode options -
+    #       0 - default - shows status manager (useful in large parsing jobs) and errors will be displayed
+    #       2 - file name only - shows file name being parsed, and errors only
+
+    #   for purpose of this example, let's change so we can see file-by-file progress
+    LLMWareConfig().set_config("debug_mode", 2)
+
+    #   this is a list of document folders that will be pulled down by calling Setup()
+    sample_folders = ["Agreements", "Invoices", "UN-Resolutions-500", "SmallLibrary", "FinDocs", "AgreementsLarge"]
+
+    library_name = "example1_library"
+
+    #   select one of the sample folders
+    selected_folder = sample_folders[0]     # e.g., "Agreements"
+
+    #   run the example
+    output = parsing_documents_into_library(library_name, selected_folder)
+
+
---
+++ fast_start/example-5-rag-semantic-query.py
@@ -0,0 +1,131 @@
+
+"""     Fast Start Example #5 - RAG with Semantic Query
+
+    This example illustrates the most common RAG retrieval pattern, which is using a semantic query, e.g.,
+    a natural language query, as the basis for retrieving relevant text chunks, and then using as
+    the context material in a prompt to ask the same question to a LLM.
+
+    In this example, we will show the following:
+
+    1.  Create library and install embeddings (feel free to skip / substitute a library created in an earlier step).
+    2.  Ask a general semantic query to the entire library collection.
+    3.  Select the most relevant results by document.
+    4.  Loop through all of the documents - packaging the context and asking our questions to the LLM.
+
+"""
+
+
+import os
+from llmware.library import Library
+from llmware.retrieval import Query
+from llmware.setup import Setup
+from llmware.status import Status
+from llmware.prompts import Prompt
+from llmware.configs import LLMWareConfig
+
+
+def semantic_rag (library_name, embedding_model_name, llm_model_name):
+
+    """ Illustrates the use of semantic embedding vectors in a RAG workflow
+        --self-contained example - will be duplicative with some of the steps taken in other examples """
+
+    # Step 1 - Create library which is the main 'organizing construct' in llmware
+    print ("\nupdate: Step 1 - Creating library: {}".format(library_name))
+
+    library = Library().create_new_library(library_name)
+
+    # Step 2 - Pull down the sample files from S3 through the .load_sample_files() command
+    #   --note: if you need to refresh the sample files, set 'over_write=True'
+    print ("update: Step 2 - Downloading Sample Files")
+
+    sample_files_path = Setup().load_sample_files(over_write=False)
+    contracts_path = os.path.join(sample_files_path, "Agreements")
+
+    # Step 3 - point ".add_files" method to the folder of documents that was just created
+    #   this method parses all of the documents, text chunks, and captures in MongoDB
+    print("update: Step 3 - Parsing and Text Indexing Files")
+
+    library.add_files(input_folder_path=contracts_path)
+
+    # Step 4 - Install the embeddings
+    print("\nupdate: Step 4 - Generating Embeddings in {} db - with Model- {}".format(vector_db, embedding_model))
+
+    library.install_new_embedding(embedding_model_name=embedding_model_name, vector_db=vector_db)
+
+    # RAG steps start here ...
+
+    print("\nupdate: Loading model for LLM inference - ", llm_model_name)
+
+    prompter = Prompt().load_model(llm_model_name)
+
+    query = "what is the executive's base annual salary"
+
+    #   key step: run semantic query against the library and get all of the top results
+    results = Query(library).semantic_query(query, result_count=50, embedding_distance_threshold=1.0)
+
+    #   if you want to look at 'results', uncomment the two lines below
+    #   for i, res in enumerate(results):
+    #       print("update: ", i, res["file_source"], res["distance"], res["text"])
+
+    for i, contract in enumerate(os.listdir(contracts_path)):
+
+        qr = []
+
+        if contract != ".DS_Store":
+
+            print("\nContract Name: ", i, contract)
+
+            #   we will look through the list of semantic query results, and pull the top results for each file
+            for j, entries in enumerate(results):
+
+                if entries["file_source"] == contract:
+                    print("Top Retrieval: ", j, entries["distance"], entries["text"])
+                    qr.append(entries)
+
+            #   we will add the query results to the prompt
+            source = prompter.add_source_query_results(query_results=qr)
+
+            #   run the prompt
+            response = prompter.prompt_with_source(query, prompt_name="default_with_context", temperature=0.3)
+
+            #   note: prompt_with_resource returns a list of dictionary responses
+            #   -- depending upon the size of the source context, it may call the llm several times
+            #   -- each dict entry represents 1 call to the LLM
+
+            for resp in response:
+                if "llm_response" in resp:
+                    print("\nupdate: llm answer - ", resp["llm_response"])
+
+            # start fresh for next document
+            prompter.clear_source_materials()
+
+    return 0
+
+
+if __name__ == "__main__":
+
+    LLMWareConfig().set_active_db("sqlite")
+
+    #   for this example, we will use an embedding model that has been 'fine-tuned' for contracts
+    embedding_model = "industry-bert-contracts"
+
+    #   note: "faiss" is being used for fast start / no set-up required - we would recommend installing
+    #   a proper vector db for greater scalability and performance
+    vector_db = "faiss"
+
+    # pick any name for the library
+    lib_name = "example5_library"
+
+    example_models = ["llmware/bling-1b-0.1", "llmware/bling-tiny-llama-v0", "llmware/dragon-yi-6b-gguf"]
+
+    # use local cpu model
+    llm_model_name = example_models[0]
+
+    #   to swap in a gpt-4 openai model - uncomment these two lines
+    #   llm_model_name = "gpt-4"
+    #   os.environ["USER_MANAGED_OPENAI_API_KEY"] = "<insert-your-openai-key>"
+
+    semantic_rag(lib_name, embedding_model, llm_model_name)
+
+
+
