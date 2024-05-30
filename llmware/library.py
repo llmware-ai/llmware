a8c22b2daf39429db4c12125431f91c7c6104d32
Will Taner <72456136+willtaner@users.noreply.github.com>
2024-05-29 21:03:38 -0400

    Update llmware/library.py
    Co-authored-by: MacOS <bachhofner.dev@gmail.com>

--- llmware/library.py
+++ llmware/library.py
@@ -585,7 +585,7 @@ class Library:
                    table_grid=True, get_header_text=True, table_strategy=1, strip_header=False,
                    verbose_level=2, copy_files_to_library=True):
 
-        """ Main method to integrate documents into a Library - pass a local filepath folder and all files will be
+        """Main method to integrate documents into a Library - pass a local filepath folder and all files will be
         routed to appropriate parser by file type extension 
         
             Parameters
