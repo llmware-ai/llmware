0f23b5dafe068e47a13bd4fa6f83ecd96c225514
Darren Oberst <darrenoberst@gmail.com>
2026-01-04 02:40:23 -0800

    update welcome scripts windows and mac

--- welcome_to_llmware.sh
+++ welcome_to_llmware.sh
@@ -12,18 +12,16 @@ pip3 install -r ./llmware/requirements_extras.txt
 # Move selected examples into root path for easy execution from command line
 scp ./examples/Getting_Started/welcome_example.py .
 scp ./fast_start/rag/*.py .
+scp ./fast_start/agents/*.py .
 scp ./examples/UI/gguf_streaming_chatbot.py .
 scp ./examples/SLIM-Agents/agent-llmfx-getting-started.py .
 scp ./examples/SLIM-Agents/using_slim_extract_model.py .
-scp ./examples/SLIM-Agents/using_slim_summary.py .
 scp ./examples/Models/bling_fast_start.py .
 scp ./examples/Models/dragon_gguf_fast_start.py .
-scp ./examples/Prompts/document_summarizer.py .
 scp ./examples/Use_Cases/web_services_slim_fx.py .
 scp ./examples/Use_Cases/invoice_processing.py .
 scp ./examples/Models/using-whisper-cpp-sample-files.py .
 scp ./examples/Parsing/parsing_microsoft_ir_docs.py .
-scp ./examples/Models/chat_models_gguf_fast_start.py .
 scp ./examples/Models/gguf_streaming.py .
 
 echo "\nWelcome Steps Completed"
@@ -40,7 +38,6 @@ echo "    -- example-6-rag-multi-step-query.py"
 echo "    -- gguf_streaming.py"
 echo "    -- bling_fast_start.py"
 echo "    -- using_slim_extract_model.py"
-echo "    -- using_slim_summary.py"
 echo "    -- dragon_gguf_fast_start.py"
 echo "    -- invoice_processing.py"
 echo "    -- gguf_streaming_chatbot.py"
@@ -48,8 +45,6 @@ echo "    -- agent-llmfx-getting-started.py"
 echo "    -- whisper-cpp-sample-files.py"
 echo "    -- web_services_slim_fx.py"
 echo "    -- parsing_microsoft_ir_docs.py"
-echo "    -- document_summarizer.py"
-echo "    -- chat_models_gguf_fast_start.py"
 
 echo ""
 echo "To run an example from command-line:  python3 welcome_example.py"
